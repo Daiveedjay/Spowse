c300c895069e1113170b4c762efb0ca4c80cb48f
Daiveed <jajadavidjid@gmail.com>
2023-01-26 23:13:31 +0100

    Modal window added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
 "use strict";
 
 // Selects Elements
+const heroSection = document.querySelector(".hero__section");
 const emailInput = document.querySelector("#email-input");
 const errorMessage = document.querySelector(".error--message");
 const submit = document.querySelector(".submit");
-
+const modalMessage = document.querySelector(".sucess--modal");
 // Adds an event Listener to the button which verifies the email address
 submit.addEventListener("click", function (e) {
   e.preventDefault();
+
   const email = emailInput.value;
   const emailRegex =
     /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -19,5 +21,11 @@ submit.addEventListener("click", function (e) {
     }, 2000);
   } else {
     errorMessage.style.display = "none";
+    modalMessage.classList.toggle("toggle--display");
+    heroSection.classList.toggle("overlay");
+    setTimeout(function () {
+      modalMessage.classList.toggle("toggle--display");
+      heroSection.classList.toggle("overlay");
+    }, 2000);
   }
 });
